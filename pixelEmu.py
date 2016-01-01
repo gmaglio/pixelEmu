4141adbfb80e3934f042725d511a941858b694d3
gmaglio <gmaglio@gmail..com>
2016-01-01 16:39:53 -0500

    implemented put_pixel

--- pixelEmu.py
+++ pixelEmu.py
@@ -10,13 +10,21 @@ black = 0, 0, 0
 red = 255, 0, 0
 rect = 0, 0, 640/matrixArea[0], 480/matrixArea[1] 
 
+def put_pixel((x, y),(r,g,b)):
+    pygame.draw.rect(screen, (r,g,b), (x, y, 640/matrixArea[0], 480/matrixArea[1]) )
+    pygame.display.flip()
+
 screen = pygame.display.set_mode(screenSize)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    for y in range(32):
+        for x in range(64):
+            put_pixel((x,y), (x, y, y))
 
     screen.fill(black)
-    pygame.draw.rect(screen, red, rect)
-    pygame.display.flip()
+
+
+    
